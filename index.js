90c1b93e90353e3a3614130a032c6124fc69c3b0
sydneyfriedman <friedman.sydneyl@gmail.com>
2024-01-25 14:44:02 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+const dodgerWidth = 40;
+const spaceWidth = 400;
+
+const maxRightValue = spaceWidth - dodgerWidth;
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < maxRightValue) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
